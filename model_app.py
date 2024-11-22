da5dc99c90a4c9ab93adc0936b8edbfe2cc235bd
aravindh-c <61825537+aravindh-c@users.noreply.github.com>
2024-11-22 19:02:17 +0530

    model_app fixed

--- model_app.py
+++ model_app.py
@@ -1,6 +1,6 @@
 # importing libraires
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 import pandas as pd
 import joblib
@@ -16,9 +16,10 @@ class Input(BaseModel):
     no_of_trainings: int
     age: int
     previous_year_rating: int
-    KPIs_met : int
-    awards_won: int
+    KPIs_met : int = Field(alias="KPIs_met >80%", default=None)
+    awards_won: int = Field(alias="awards_won?", default=None)
     avg_training_score: int
+    length_of_service: int
 
 
 
@@ -29,11 +30,11 @@ class Output(BaseModel):
 def predict(data: Input) -> Output:
     X_input = pd.DataFrame([[data.department,data.region,data.education,data.gender,
               data.recruitment_channel,data.no_of_trainings,data.age,data.previous_year_rating,
-              data.KPIs_met,data.awards_won,data.avg_training_score]])
+              data.KPIs_met,data.awards_won,data.avg_training_score,data.length_of_service]])
 
     X_input.columns = ['department','region','education','gender','recruitment_channel',
-                       'no_of_trainings','age','previous_year_rating','KPIs_met >80%','awards_won',
-                       'avg_training_score']
+                       'no_of_trainings','age','previous_year_rating','KPIs_met >80%','awards_won?',
+                       'avg_training_score','length_of_service']
 
     #load model
     model = joblib.load('promotion_pipeline_model.pkl')
@@ -42,4 +43,4 @@ def predict(data: Input) -> Output:
     prediction = model.predict(X_input)
 
     #output
-    return Output(target = prediction)
+    return Output(is_promoted = prediction)
